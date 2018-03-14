d8bb1a752e12695219b48be9e7d2b2fcd3f858c1
hikaen2 <hikaen2@example.com>
2018-03-14 23:35:54 +0900

    mod

--- tenuki.h
+++ tenuki.h
@@ -27,145 +27,6 @@ namespace tenuki {
     using move_t = uint16_t;
 
     /**
-     * 手番
-     */
-    namespace side {
-        const side_t BLACK = 0; // 先手
-        const side_t WHITE = 1; // 後手
-    }
-
-    /**
-     * 駒の種類
-     * 8を足すと成る. 5以下が成れる.
-     */
-    namespace type {
-        const type_t PAWN            = 0;  // 歩
-        const type_t LANCE           = 1;  // 香
-        const type_t KNIGHT          = 2;  // 桂
-        const type_t SILVER          = 3;  // 銀
-        const type_t BISHOP          = 4;  // 角
-        const type_t ROOK            = 5;  // 飛
-        const type_t GOLD            = 6;  // 金
-        const type_t KING            = 7;  // 王
-        const type_t PROMOTED_PAWN   = 8;  // と
-        const type_t PROMOTED_LANCE  = 9;  // 成香
-        const type_t PROMOTED_KNIGHT = 10; // 成桂
-        const type_t PROMOTED_SILVER = 11; // 成銀
-        const type_t PROMOTED_BISHOP = 12; // 馬
-        const type_t PROMOTED_ROOK   = 13; // 龍
-        const type_t EMPTY           = 14; // 空
-        const type_t WALL            = 15; // 壁
-    }
-
-    /**
-     * 升
-     * 1xxxxxxx WALL
-     * x1xxxxxx EMPTY
-     * xx1xxxxx BLACK
-     * xxx1xxxx WHITE
-     * xxxx1111 type
-     */
-    namespace square {
-        const square_t W                 = 0b00010000;
-        const square_t B_PAWN            = 0;
-        const square_t B_LANCE           = 1;
-        const square_t B_KNIGHT          = 2;
-        const square_t B_SILVER          = 3;
-        const square_t B_BISHOP          = 4;
-        const square_t B_ROOK            = 5;
-        const square_t B_GOLD            = 6;
-        const square_t B_KING            = 7;
-        const square_t B_PROMOTED_PAWN   = 8;
-        const square_t B_PROMOTED_LANCE  = 9;
-        const square_t B_PROMOTED_KNIGHT = 10;
-        const square_t B_PROMOTED_SILVER = 11;
-        const square_t B_PROMOTED_BISHOP = 12;
-        const square_t B_PROMOTED_ROOK   = 13;
-        const square_t EMPTY             = 14;
-        const square_t WALL              = 15;
-        const square_t W_PAWN            = W | 0;
-        const square_t W_LANCE           = W | 1;
-        const square_t W_KNIGHT          = W | 2;
-        const square_t W_SILVER          = W | 3;
-        const square_t W_BISHOP          = W | 4;
-        const square_t W_ROOK            = W | 5;
-        const square_t W_GOLD            = W | 6;
-        const square_t W_KING            = W | 7;
-        const square_t W_PROMOTED_PAWN   = W | 8;
-        const square_t W_PROMOTED_LANCE  = W | 9;
-        const square_t W_PROMOTED_KNIGHT = W | 10;
-        const square_t W_PROMOTED_SILVER = W | 11;
-        const square_t W_PROMOTED_BISHOP = W | 12;
-        const square_t W_PROMOTED_ROOK   = W | 13;
-
-        inline bool is_black(square_t sq) {
-            return sq <= square::B_PROMOTED_ROOK;
-        }
-
-        inline bool is_white(square_t sq) {
-            return sq >= square::W_PAWN;
-        }
-
-        inline bool is_friend(square_t sq, side_t s) {
-            return s == side::BLACK ? is_black(sq) : is_white(sq);
-        }
-
-        inline bool is_enemy(square_t sq, side_t s) {
-            return s == side::BLACK ? is_white(sq) : is_black(sq);
-        }
-
-        inline type_t type_of(square_t sq) {
-            assert(sq != square::EMPTY);
-            return sq & 0b00001111;
-        }
-
-        inline square_t promote(square_t sq) {
-            return sq | 0b00001000;
-        }
-
-        inline square_t unpromote(square_t sq) {
-            return sq & 0b11110111;
-        }
-    }
-
-    /**
-     * 手
-     * 1xxxxxxx xxxxxxxx promote
-     * x1xxxxxx xxxxxxxx drop
-     * xx111111 1xxxxxxx from
-     * xxxxxxxx x1111111 to
-     */
-    namespace move {
-        inline move_t create(int from, int to) {
-            return from << 7 | to;
-        }
-
-        inline move_t create_promote(int from, int to) {
-            return from << 7 | to | 0b1000000000000000;
-        }
-
-        inline move_t create_drop(type_t t, int to) {
-            return t << 7 | to | 0b0100000000000000;
-        }
-
-        inline int from(move_t m) {
-            return (m >> 7) & 0b01111111;
-        }
-
-        inline int to(move_t m) {
-            return m & 0b01111111;
-        }
-
-        inline bool is_promote(move_t m) {
-            return (m & 0b1000000000000000) != 0;
-        }
-
-        inline bool is_drop(move_t m) {
-            return (m & 0b0100000000000000) != 0;
-        }
-    }
-
-    /**
      * 局面
      */
     struct position {
@@ -186,9 +47,106 @@ namespace tenuki {
          * 110
          */
         square_t squares[111];
-        uint8_t pieces_in_hand[2][8]; ///< [side][type]
-        side_t side_to_move;          ///< 手番
+        uint8_t pieces_in_hand[2][8]; // [side_t][type_t]
+        side_t side_to_move;          // 手番
     };
+
+    /**
+     * 手番
+     */
+    namespace side {
+        constexpr side_t BLACK = 0; // 先手
+        constexpr side_t WHITE = 1; // 後手
+    }
+
+    /**
+     * 駒の種類
+     * 8を足すと成る. 5以下が成れる.
+     */
+    namespace type {
+        constexpr type_t PAWN            = 0;  // 歩
+        constexpr type_t LANCE           = 1;  // 香
+        constexpr type_t KNIGHT          = 2;  // 桂
+        constexpr type_t SILVER          = 3;  // 銀
+        constexpr type_t BISHOP          = 4;  // 角
+        constexpr type_t ROOK            = 5;  // 飛
+        constexpr type_t GOLD            = 6;  // 金
+        constexpr type_t KING            = 7;  // 王
+        constexpr type_t PROMOTED_PAWN   = 8;  // と
+        constexpr type_t PROMOTED_LANCE  = 9;  // 成香
+        constexpr type_t PROMOTED_KNIGHT = 10; // 成桂
+        constexpr type_t PROMOTED_SILVER = 11; // 成銀
+        constexpr type_t PROMOTED_BISHOP = 12; // 馬
+        constexpr type_t PROMOTED_ROOK   = 13; // 龍
+        constexpr type_t EMPTY           = 14; // 空
+        constexpr type_t WALL            = 15; // 壁
+    }
+
+    /**
+     * 升
+     * 1xxxxxxx WALL
+     * x1xxxxxx EMPTY
+     * xx1xxxxx BLACK
+     * xxx1xxxx WHITE
+     * xxxx1111 type
+     */
+    namespace square {
+        constexpr square_t W                 = 0b00010000;
+        constexpr square_t B_PAWN            = 0;
+        constexpr square_t B_LANCE           = 1;
+        constexpr square_t B_KNIGHT          = 2;
+        constexpr square_t B_SILVER          = 3;
+        constexpr square_t B_BISHOP          = 4;
+        constexpr square_t B_ROOK            = 5;
+        constexpr square_t B_GOLD            = 6;
+        constexpr square_t B_KING            = 7;
+        constexpr square_t B_PROMOTED_PAWN   = 8;
+        constexpr square_t B_PROMOTED_LANCE  = 9;
+        constexpr square_t B_PROMOTED_KNIGHT = 10;
+        constexpr square_t B_PROMOTED_SILVER = 11;
+        constexpr square_t B_PROMOTED_BISHOP = 12;
+        constexpr square_t B_PROMOTED_ROOK   = 13;
+        constexpr square_t EMPTY             = 14;
+        constexpr square_t WALL              = 15;
+        constexpr square_t W_PAWN            = W | 0;
+        constexpr square_t W_LANCE           = W | 1;
+        constexpr square_t W_KNIGHT          = W | 2;
+        constexpr square_t W_SILVER          = W | 3;
+        constexpr square_t W_BISHOP          = W | 4;
+        constexpr square_t W_ROOK            = W | 5;
+        constexpr square_t W_GOLD            = W | 6;
+        constexpr square_t W_KING            = W | 7;
+        constexpr square_t W_PROMOTED_PAWN   = W | 8;
+        constexpr square_t W_PROMOTED_LANCE  = W | 9;
+        constexpr square_t W_PROMOTED_KNIGHT = W | 10;
+        constexpr square_t W_PROMOTED_SILVER = W | 11;
+        constexpr square_t W_PROMOTED_BISHOP = W | 12;
+        constexpr square_t W_PROMOTED_ROOK   = W | 13;
+        constexpr bool is_black(square_t sq) { return sq <= square::B_PROMOTED_ROOK; }
+        constexpr bool is_white(square_t sq) { return sq >= square::W_PAWN; }
+        constexpr bool is_friend(square_t sq, side_t s) { return s == side::BLACK ? is_black(sq) : is_white(sq); }
+        constexpr bool is_enemy(square_t sq, side_t s) { return s == side::BLACK ? is_white(sq) : is_black(sq); }
+        constexpr type_t type_of(square_t sq) { return sq & 0b00001111; }
+        constexpr square_t promote(square_t sq) { return sq | 0b00001000; }
+        constexpr square_t unpromote(square_t sq) { return sq & 0b11110111; }
+    }
+
+    /**
+     * 手
+     * 1xxxxxxx xxxxxxxx promote
+     * x1xxxxxx xxxxxxxx drop
+     * xx111111 1xxxxxxx from
+     * xxxxxxxx x1111111 to
+     */
+    namespace move {
+        constexpr move_t create(int from, int to) { return from << 7 | to; }
+        constexpr move_t create_promote(int from, int to) { return from << 7 | to | 0b1000000000000000; }
+        constexpr move_t create_drop(type_t t, int to) { return t << 7 | to | 0b0100000000000000; }
+        constexpr int from(move_t m) { return (m >> 7) & 0b01111111; }
+        constexpr int to(move_t m) { return m & 0b01111111; }
+        constexpr bool is_promote(move_t m) { return (m & 0b1000000000000000) != 0; }
+        constexpr bool is_drop(move_t m) { return (m & 0b0100000000000000) != 0; }
+    }
 
     /**
      * Direction
@@ -196,40 +154,34 @@ namespace tenuki {
      * xxxxxxx1 fly
      */
     namespace dir {
-        const dir_t N =  -1 * 2; //  -1 << 1
-        const dir_t E = -10 * 2; // -10 << 1
-        const dir_t W = +10 * 2; // +10 << 1
-        const dir_t S =  +1 * 2; //  +1 << 1
-        const dir_t NE = N + E;
-        const dir_t NW = N + W;
-        const dir_t SE = S + E;
-        const dir_t SW = S + W;
-        const dir_t NNE = N + N + E;
-        const dir_t NNW = N + N + W;
-        const dir_t FN = N | 1;
-        const dir_t FE = E | 1;
-        const dir_t FW = W | 1;
-        const dir_t FS = S | 1;
-        const dir_t FNE = NE | 1;
-        const dir_t FNW = NW | 1;
-        const dir_t FSE = SE | 1;
-        const dir_t FSW = SW | 1;
-
-        inline bool is_fly(dir_t d) {
-            return (d & 1) != 0;
-        }
-
-        inline int value(dir_t d) {
-            return d >> 1;
-        }
+        constexpr dir_t N =  -1 * 2; //  -1 << 1
+        constexpr dir_t E = -10 * 2; // -10 << 1
+        constexpr dir_t W = +10 * 2; // +10 << 1
+        constexpr dir_t S =  +1 * 2; //  +1 << 1
+        constexpr dir_t NE = N + E;
+        constexpr dir_t NW = N + W;
+        constexpr dir_t SE = S + E;
+        constexpr dir_t SW = S + W;
+        constexpr dir_t NNE = N + N + E;
+        constexpr dir_t NNW = N + N + W;
+        constexpr dir_t FN = N | 1;
+        constexpr dir_t FE = E | 1;
+        constexpr dir_t FW = W | 1;
+        constexpr dir_t FS = S | 1;
+        constexpr dir_t FNE = NE | 1;
+        constexpr dir_t FNW = NW | 1;
+        constexpr dir_t FSE = SE | 1;
+        constexpr dir_t FSW = SW | 1;
+        constexpr bool is_fly(dir_t d) { return (d & 1) != 0; }
+        constexpr int value(dir_t d) { return d >> 1; }
     }
 
-    inline int address(int file, int rank) {
+    constexpr int address(int file, int rank) {
         return file * 10 + rank;
     }
 
-    inline int file_of(int address) {
-        static const int16_t a[] = {
+    constexpr int file_of(int address) {
+        constexpr int16_t a[] = {
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
             2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
@@ -244,8 +196,8 @@ namespace tenuki {
         return a[address];
     }
 
-    inline int rank_of(int address) {
-        static const int16_t a[] = {
+    constexpr int rank_of(int address) {
+        constexpr int16_t a[] = {
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
